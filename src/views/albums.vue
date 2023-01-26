fb677b24c61b627b48ea256bc625d9a6f38ecc0e
Полина <a1@MacBook-Air-Polina.local>
2023-01-26 14:29:26 +0500

    feat: add new functions

--- src/views/albums.vue
+++ src/views/albums.vue
@@ -1,8 +1,13 @@
 <template>
     <h1>Выбрано элементов: {{ counter }}</h1><button @click="count">Посчитать</button>
-   <div v-for="ell of newAlbums">
- <input v-model="ell.active" type="checkbox"> {{ ell.id }} {{ ell.title }}</div>
- <button @click="push">Push</button>
+    <div @click="sortAlbums"> сделать сортировку</div>
+    <select>
+        <option value="inactive">inactive</option>
+        <option @click="activeButton" value="active">active</option>
+      </select>
+    <div v-for="ell of newAlbums">
+    <input v-model="ell.active" type="checkbox"> {{ ell.id }} {{ ell.title }}</div>
+   
 </template>
 <script>
 export default{
@@ -15,15 +20,13 @@ export default{
         }
     },
     methods:{
-push(){
-    this.newAlbums = this.albums.map(function(current) { 
-let object = Object.assign({}, current);
-    object.active = false;
-    return object;
-})
-console.log(this.newAlbums);
-},
-count(){
+        activeButton(){
+            this.$router.push({name:'albums', query:{status:'active'}})
+        },
+        sortAlbums(){
+        this.$router.push({name:'albums', query:{sort:'ask'}})
+      },
+        count(){
     this.counter = 0
 for(let i = 0; i< this.newAlbums.length; i++){
     if(this.newAlbums[i].active == true){
@@ -34,13 +37,27 @@ for(let k=0;k<this.newAlbums.length;k++){
     this.newAlbums[k].active = false
 }
 },
-    },
-    mounted(){
+        getAlboms(){
         fetch("https://jsonplaceholder.typicode.com/albums")
-          .then(response => response.json())
-          .then((json) =>{
-            this.albums = json
-       })
+        .then(response => response.json())
+            .then((json) =>{
+              this.newAlbums = json.map(function(current) {
+                let object = Object.assign({}, current);
+                object.active = false;
+                return object;
+            })
+              if(this.$route.query.sort === 'ask'){
+               fetch("https://jsonplaceholder.typicode.com/albums?sort=ask").then((res)=>{
+                 this.newAlbums = res
+               })
+              }else{
+                fetch("https://jsonplaceholder.typicode.com/albums?sort='desk")
+              }
+      })
+}
+    },
+   async mounted(){
+      await this.getAlboms()
     },
    
 }
--- src/views/posts.vue
+++ src/views/posts.vue
@@ -1,5 +1,10 @@
 <template>
     <div>  
+        <button @click="add">Добавить в таблицу</button>
+        <div v-if="isAddVisible" class="modal"><input class="input" v-model="title" type="textarea">
+            <input class="input" v-model="body" type="text">
+        <button class="button" @click="addItem">Добавить</button>
+        </div>
         <div v-if="isModalVisible" class="modal"><input class="input" v-model="title" type="textarea">
             <input class="input" v-model="body" type="text">
         <button class="button" @click="Change">Изменить</button></div>
@@ -18,9 +23,15 @@ return{
     body:'',
     id:'',
     isModalVisible: false,
+    isAddVisible: false,
+    newObj:{}
+
 }
 },
 methods:{
+    add(){
+        this.isAddVisible=true
+    },
     edit(object){
         this.isModalVisible= true
         this.title = object.title
@@ -31,25 +42,59 @@ methods:{
         let deleteObject = this.tableData.findIndex((ell)=> ell.id === object.id)
         console.log(deleteObject)
         this.tableData.splice(deleteObject, 1);
+        fetch( 'https://jsonplaceholder.typicode.com/posts/' + object.id, {
+        method: 'DELETE',
+      } ).then((res)=>{
+        console.log(res)
+      }).catch((e)=>{
+        console.log(e)
+      })
+    },
+  addItem(){
+    this.newObj.id = this.tableData.length+1
+    this.newObj.title=this.title
+    this.newObj.body= this.body
+    this.tableData.push(this.newObj)
+    fetch( 'https://jsonplaceholder.typicode.com/posts/', {
+        method: 'POST',
+        body: JSON.stringify(this.newObj)
+      } ).then((res)=>{
+        console.log(res)
+      }).catch((e)=>{
+        console.log(e)
+      })
+    this.newObj={}
+    this.title= ''
+    this.body=''
+    this.isAddVisible=false
     },
     Change(){
-let newObject= this.tableData.find((ell=>ell.id == this.id))
-newObject.title = this.title
-newObject.body = this.body
-this.title = ''
-this.body = ''
-this.isModalVisible= false
-
-    }
-},
-mounted(){
+    let newObject= this.tableData.find((ell=>ell.id == this.id))
+    newObject.title = this.title
+    newObject.body = this.body
+    this.title = ''
+    this.body = ''
+    this.isModalVisible= false
+    fetch( 'https://jsonplaceholder.typicode.com/posts/' + this.id, {
+        method: 'PUT',
+        body: JSON.stringify({title:this.title,body:this.body})
+      } ).then((res)=>{
+        console.log(res)
+      }).catch((e)=>{
+        console.log(e)
+      })
+    },
+  fetchTable(){
     fetch('https://jsonplaceholder.typicode.com/posts')
-          .then(response => response.json())
-          .then((json) =>{
-            this.tableData = json
-       })
-}
-}
+    .then(response => response.json())
+        .then((json) =>{
+          this.tableData = json
+        })
+  }
+},
+ mounted(){
+  this.fetchTable()
+    }}
 </script>
 <style>
  .modal{
